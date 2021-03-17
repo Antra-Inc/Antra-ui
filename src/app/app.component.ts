40e2591f056065051e9a3296a92df0db5141f88c
narendar.gentyala <narendar.gentyala@antra.com>
2021-03-17 19:30:04 +0530

    on leave showing children items fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,59 +9,56 @@ import { NavLinkNode, NavLinkNodeFlat } from 'antra-ui';
 })
 export class AppComponent {
   isOpen = true;
-  sideNavConfig: NavLinkNode[] = [{
-    name: 'Clock',
-    icon: 'alarm',
-  },
-  {
-    name: 'Buttons',
-    icon: 'outbond',
-    children: [
-      { name: 'ButtonComponent', url: 'components/button' },
-      { name: 'RaisedButtonComponent', url: 'components/raised-button' },
-    ],
-  },
-  {
-    name: 'SideNav',
-    icon: 'settings',
-    useSvgIcon: true,
-    children: [{ name: 'SidenavbarComponent', url: 'components/sidenav' }],
-  },
-  {
-    name: 'Widgets',
-    icon: 'featured_play_list',
-    children: [{ name: 'WidgetComponent', url: 'components/widget' }],
-  },
-  {
-    name: 'Login',
-    icon: 'login',
-    children: [{ name: 'LoginComponent', url: 'components/login' }],
-  },
-  {
-    name: 'Password Reset',
-    icon: 'lock_open',
-    children: [
-      { name: 'PasswordResetComponent', url: 'components/password-reset' },
-      { name: 'ChangePasswordComponent', url: 'components/change-password' },
-    ],
-  },
-  {
-    name: 'Notification Dialog',
-    icon: 'add_alert',
-    children: [
-      { name: 'NotificationDialogComponent', url: 'components/notification-dialog' },
-    ],
-  },
-  {
-    name: 'Logo',
-    icon: 'fingerprint',
-    children: [{ name: 'LogoComponent', url: 'components/logo' }],
-  },
-  {
-    name: 'Progress Spinner',
-    icon: 'donut_large',
-    children: [{ name: 'ProgressSpinnerComponent', url: 'components/progress-spinner' }],
-  },
+  sideNavConfig: NavLinkNode[] = [
+    {
+      name: 'Buttons',
+      icon: 'outbond',
+      children: [
+        { name: 'ButtonComponent', url: 'components/button' },
+        { name: 'RaisedButtonComponent', url: 'components/raised-button' },
+      ],
+    },
+    {
+      name: 'SideNav',
+      icon: 'settings',
+      useSvgIcon: true,
+      children: [{ name: 'SidenavbarComponent', url: 'components/sidenav' }],
+    },
+    {
+      name: 'Widgets',
+      icon: 'featured_play_list',
+      children: [{ name: 'WidgetComponent', url: 'components/widget' }],
+    },
+    {
+      name: 'Login',
+      icon: 'login',
+      children: [{ name: 'LoginComponent', url: 'components/login' }],
+    },
+    {
+      name: 'Password Reset',
+      icon: 'lock_open',
+      children: [
+        { name: 'PasswordResetComponent', url: 'components/password-reset' },
+        { name: 'ChangePasswordComponent', url: 'components/change-password' },
+      ],
+    },
+    {
+      name: 'Notification Dialog',
+      icon: 'add_alert',
+      children: [
+        { name: 'NotificationDialogComponent', url: 'components/notification-dialog' },
+      ],
+    },
+    {
+      name: 'Logo',
+      icon: 'fingerprint',
+      children: [{ name: 'LogoComponent', url: 'components/logo' }],
+    },
+    {
+      name: 'Progress Spinner',
+      icon: 'donut_large',
+      children: [{ name: 'ProgressSpinnerComponent', url: 'components/progress-spinner' }],
+    },
   ];
 
   constructor(private router: Router) { }
